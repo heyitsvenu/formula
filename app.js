c2c2f039785416da3362c827862c6185e5d0333c
Venu <venu6696@gmail.com>
2022-01-26 16:22:31 +0530

    update UI

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const submit = document.querySelector('#submit');
 const result = document.querySelector('.result');
 const degreeInput = document.querySelector('#degree');
 const minutesInput = document.querySelector('#minutes');
@@ -6,28 +5,18 @@ const secondsInput = document.querySelector('#seconds');
 const add = document.querySelector('#add');
 const sub = document.querySelector('#sub');
 
-let isAdd = false;
-
-function handleSubmit(e) {
+function handleAddSubmit(e) {
   e.preventDefault();
   const degreeInputValue = degreeInput.value;
   const minutesInputValue = minutesInput.value;
   const secondsInputValue = secondsInput.value;
 
   let formulaResult;
-  if (!isAdd) {
-    formulaResult =
-      360 -
-      (Number(degreeInputValue) +
-        +(Number(minutesInputValue) / 60).toFixed(2) +
-        +(Number(secondsInputValue) / 3600).toFixed(4));
-  } else {
-    formulaResult =
-      360 +
-      (Number(degreeInputValue) +
-        +(Number(minutesInputValue) / 60).toFixed(2) +
-        +(Number(secondsInputValue) / 3600).toFixed(4));
-  }
+  formulaResult =
+    360 +
+    (Number(degreeInputValue) +
+      +(Number(minutesInputValue) / 60).toFixed(2) +
+      +(Number(secondsInputValue) / 3600).toFixed(4));
 
   const resultDegree = Math.floor(formulaResult);
   const remainderDegree = formulaResult - Math.floor(formulaResult);
@@ -48,13 +37,38 @@ function handleSubmit(e) {
   secondsInput.value = '';
 }
 
-submit.addEventListener('click', handleSubmit);
-add.addEventListener('click', (e) => {
+function handleSubSubmit(e) {
   e.preventDefault();
-  isAdd = true;
-  console.log(isAdd);
-});
-sub.addEventListener('click', (e) => {
-  e.preventDefault();
-  isAdd = false;
-});
+  const degreeInputValue = degreeInput.value;
+  const minutesInputValue = minutesInput.value;
+  const secondsInputValue = secondsInput.value;
+
+  let formulaResult;
+  formulaResult =
+    360 -
+    (Number(degreeInputValue) +
+      +(Number(minutesInputValue) / 60).toFixed(2) +
+      +(Number(secondsInputValue) / 3600).toFixed(4));
+
+  const resultDegree = Math.floor(formulaResult);
+  const remainderDegree = formulaResult - Math.floor(formulaResult);
+  const resultMinutes = remainderDegree.toFixed(4) * 60;
+  const remainderMinutes = resultMinutes - Math.floor(resultMinutes);
+  const resultSeconds = remainderMinutes.toFixed(3) * 60;
+
+  const finalResult = `Result : ${resultDegree}&deg; + ${Math.floor(
+    resultMinutes
+  )}' + ${resultSeconds}"`;
+
+  console.log(finalResult);
+
+  result.innerHTML = finalResult;
+
+  degreeInput.value = '';
+  minutesInput.value = '';
+  secondsInput.value = '';
+}
+
+add.addEventListener('click', handleAddSubmit);
+
+sub.addEventListener('click', handleSubSubmit);
